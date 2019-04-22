2bf9f8aee968620e01d09722daf0aeda59cd393b
A Shendage <shendageanand7@gmail.com>
2019-04-22 15:58:54 +0530

    Initial commit

---
+++ config/compass.rb
@@ -0,0 +1,22 @@
+
+
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
---
+++ js/main.js
@@ -0,0 +1,29 @@
+function clearData () {
+    document.getElementById('result').value=""
+}
+
+function clickedvalue(me) {
+    document.getElementById('result').value+=me;
+}
+
+function solve(){
+    if(document.getElementById('output').value === ""){
+        e.preventDefault(e);
+    }
+    else{
+        let x =  document.getElementById('result').value;
+        let y = eval(x);
+        document.getElementById('output').innerHTML = y;
+    }
+    
+}
+function sliceValue(){
+    let str = document.getElementById('result').value;
+    let slicedItem = str.slice(0, -1);
+    document.getElementById('result').value = slicedItem;
+}
+
+function addClass() {
+    document.getElementById('calc').classList.toggle('dark-theme');
+    document.getElementById("switch-img").src="assets/day-mode.png";
+}
